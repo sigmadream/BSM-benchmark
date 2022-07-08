830595c8c6a436112ceac4e929d1db8d17ecef85
Sangkon Han <sigmadream@gmail.com>
2022-07-08 13:56:14 +0900

    // wip

--- bsm-and-cqrs/use_db.py
+++ bsm-and-cqrs/use_db.py
@@ -1,7 +1,18 @@
-from sqlalchemy.orm import declarative_base
+import sqlite3
+import time
 
-Base = declarative_base()
+import pandas as pd
 
-
-class BSM(Base):
-    __tablename__ = "bsm"
+if __name__ == '__main__':
+    conn = sqlite3.connect('bsm.db')
+    c = conn.cursor()
+    chunksize = 10 ** 6
+    start = time.time()
+    with pd.read_csv('data/Wyoming_CV_Pilot_Basic_Safety_Message_One_Day_Sample.csv', chunksize=chunksize,
+                     low_memory=False) as reader:
+        for chunk in reader:
+            chunk.to_sql('bsm', conn, if_exists='append', index=False)
+    print("time :", time.time() - start)
+    # time : 1117.6711807250977
+    # C:\Users\sigma\works\practice-sql\bsm-and-cqrs\venv\Scripts\python.exe C:/Users/sigma/works/practice-sql/bsm-and-cqrs/use_db.py
+    # time : 1117.6711807250977
